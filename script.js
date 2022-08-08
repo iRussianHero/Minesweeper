e6b7c46d99cf295ef3aee27969d0c8862784b713
Yakushev Roman <boxboxbox@inbox.ru>
2022-08-08 12:43:30 +0300

    Add function checkResult

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-startGame(8,8,15);
+startGame(8,8,1);
 
 function startGame(width, height, bombs_count){
     const field = document.querySelector('.field');
     const cellsCount = width * height;
     field.innerHTML = '<button></button>'.repeat(cellsCount);
     const cells = [...field.children];
+    let closedCount = cellsCount;
 
     const bombs = [...Array(cellsCount).keys()]
     .sort(() => Math.random() - 0.5)
@@ -21,9 +22,16 @@ function startGame(width, height, bombs_count){
         open(row, column);
     });
 
-    function getCount(row, column){
+    function isValid(row, column) {
+        return row >= 0
+        && row < height
+        && column >= 0
+        && column < width;
+    }
+
+    function getCount(row, column) {
         let count = 0;
-        for (let x = -1; x <= 1; x++){
+        for (let x = -1; x <= 1; x++) {
             for (let y = -1; y <= 1; y++) {
                 if (isBomb(row + y, column + x)){
                     count++;
@@ -34,13 +42,42 @@ function startGame(width, height, bombs_count){
     }
     
     function open(row, column) {
+        if(!isValid(row, column)) return;
+
         const index = row * width + column;
         const cell = cells[index];
-        cell.innerHTML = isBomb(row, column) ? 'X' : getCount(row, column);
+
+        if(cell.disabled === true) return;
+
         cell.disabled = true;
+        
+        if (isBomb(row, column)) {          
+            cell.innerHTML = 'X';
+            alert('Вы проиграли...');
+            return;
+        }
+        
+        closedCount--;
+        if (closedCount <= bombs_count) {
+            alert('Вы победили!');
+            return;
+        }
+
+        const count = getCount(row, column);
+        if (count !== 0) {
+            cell.innerHTML = count;
+            return;
+        }
+
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                open(row + y, column + x);    
+            }         
+        }
     }
 
     function isBomb(row, column){
+        if (!isValid(row, column)) return false;
         const index = row * width + column;
         return bombs.includes(index);
     }
